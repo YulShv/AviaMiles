9b960d3fa6be5a03f1524c956f5d5230cd5973d5
YulShv <yuli.shv@gmail.com>
2022-07-30 15:56:41 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 8020;
+        int milePrice = 20;
+        int bonusMiles = ticketPrice / milePrice;
+
+        System.out.println ("Количество бонусных миль: " + bonusMiles);
+
+    }
+}
